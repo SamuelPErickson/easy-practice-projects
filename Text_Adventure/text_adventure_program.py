8acc68f247529ae20c281d913d1a8251fa3f3361
SamuelPErickson <Samuel.P.Erickson@gmail.com>
2017-04-09 19:48:27 -0500

    text adventure addition

---
+++ Text_Adventure/text_adventure_program.py
@@ -0,0 +1,27 @@
+outcome_1 = "Your org is the bomb"
+outcome_2 = "You have great growth, some coordination"
+outcome_3 = "You have great growth, no coordination"
+outcome_4 = "You have some growth, some coordination"
+outcome_5 = "You have some growth, no coordination"
+outcome_6 = "You have some growth, great coordination"
+outcome_7 = "You have no growth, great coordination"
+outcome_8 = "You have no growth, some coordination"
+outcome_9 = "You have no growth, no coordination"
+
+intro = input ("You will be asked a series of questions to determine how your ogranization is set up. \n"
+               "Enter either a or b. Press enter to begin.")
+
+question_1 = input ("What kind of work does your department have student workers doing? \n"
+                    "a.) Only basic tasks. \n"
+                    "b.) They start with basic tasks but can more up to more complicated things.\n")
+question_2 = input ("How often do student workers move around your deaprtment? \n"
+                    "a.) Students often move areas if they find interest in the work of other areas. \n"
+                    "b.) Students rarely move out of the area they were hired into.\n")
+question_3 = input ("Student turnover is constant. How do you handle this? \n"
+                    "a.) The manager for each student team fills openings as they become available. \n"
+                    "b.) There is a specific person in charge of hiring students for the department.\n")
+question_4 = input ("Different teams have busy periods at different times of the year. "
+                    "How are student worker resources managed for this? \n"
+                    "a.) Students are moved between teams based off the ebb and flow of work. \n"
+                    "b.) Each team hires students to different degrees and for different durations.\n")
+
